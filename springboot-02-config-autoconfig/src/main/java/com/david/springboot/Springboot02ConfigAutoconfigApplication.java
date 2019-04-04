425498415414469f800c6a044b445c101cc40c62
yuanwei <wei.yuan@gh-ca.com>
2019-04-05 00:21:17 +0800

    自动配置解析

---
+++ springboot-02-config-autoconfig/src/main/java/com/david/springboot/Springboot02ConfigAutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.david.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02ConfigAutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02ConfigAutoconfigApplication.class, args);
+	}
+
+}
